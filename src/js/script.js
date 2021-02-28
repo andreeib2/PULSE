5ee27b13440aa839c54f956ee03b20a596e21025
Andrey <andreeib@mail.ru>
2021-02-28 21:29:15 +0300

    valited2

--- src/js/script.js
+++ src/js/script.js
@@ -92,7 +92,25 @@ $(document).ready(function(){
     valideForms('#consultation form');
     valideForms('#order form');
 
-    
+    $('inpunt[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("inpunt").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });    
 });
 
 
